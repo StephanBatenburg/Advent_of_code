0e61bc81ec606b340ec7273ea0ac70fd7f4b6200
STEPHAN_LAPTOP\baten <batenburg.s@live.nl>
2024-12-03 20:22:52 +0100

    Still not handling all edge cases.

--- 2024/dayTwo.java
+++ 2024/dayTwo.java
@@ -50,33 +50,129 @@ class dayTwo{
             }
         }
 
-        System.out.println(safeLevels);
+        System.out.println("First part answer: " + safeLevels);
 
         //second part:
         safeLevels = 0;
         for(int[] levels : levelslList){
-            int falseLevelCounter = 0;
+            //boolean array: decreasing, increasing, noissues
+            boolean[] checks = {true, true, true};
             int previousLevel = 0;
-            for(int level : levels){
-                if(previousLevel == 0){
-                    previousLevel = level;
-                }else if(previousLevel == level){
-                    falseLevelCounter++;
-                }else if(((previousLevel - level) >= 4) || ((level-previousLevel) >= 4)){
-                    falseLevelCounter++;
-                }else if(previousLevel > level){
-                    falseLevelCounter++;
-                }else if(previousLevel < level){
-                    falseLevelCounter++;
+
+            int falseIndex = -1;
+
+            for(int i = 0; i < levels.length; i++){
+                if(i == 0){
+                }else if(levels[i-1] == levels[i]){
+                    falseIndex = i;
+                    checks[2] = false;
+                    break;
+                }else if((levels[i-1] - levels[i]) >= 4){
+                    if(checks[0] && checks[1]){
+                        if((i + 1) == levels.length){
+                            falseIndex = i;
+                        }else{
+                            if(levels[i] < levels[i + 1]){
+                                falseIndex = i;
+                            }else if(levels[i] > levels[i + 1]){
+                                falseIndex = i - 1;
+                            }else{
+                                falseIndex = i;
+                            }
+                        }
+                    }else if(checks[0]){
+                        falseIndex = i;
+                    }else if(checks[1]){
+                        falseIndex = i - 1;  
+                    }
+                    checks[2] = false;
+                    break;
+                }else if((levels[i]-levels[i-1]) >= 4){
+                    if(checks[0] && checks[1]){
+                        if((i + 1) == levels.length){
+                            falseIndex = i;
+                        }else{
+                            if(levels[i] < levels[i+1]){
+                                falseIndex = i - 1;
+                            }else if(levels[i] > levels[i+1]){
+                                if((levels[i] - levels[i+1]) >= 4){
+                                    falseIndex = i;
+                                }else{
+                                    falseIndex = i - 1;
+                                }
+                            }else{
+                                falseIndex = i;
+                            }
+                        }
+                        checks[2] = false;
+                        break;
+                    }else{
+                        falseIndex = i;
+                        checks[2] = false;
+                        break;
+                    }
+                }else if((levels[i-1] > levels[i]) && (levels[i-1] != 0)){
+                    if(checks[1] == false){
+                        falseIndex = i;
+                        break;
+                    }
+                    checks[0] = false;
+                }else if(levels[i-1] < levels[i] && (levels[i-1] != 0)){
+                    if(checks[0] == false){
+                        falseIndex = i - 1;
+                        break;
+                    }
+                    checks[1] = false;
                 }
-                previousLevel = level;
             }
-            if(falseLevelCounter < 2){
+
+            if((checks[0] || checks[1]) && checks[2]){
                 safeLevels++;
+                System.out.println("Passed level: " + Arrays.toString(levels));
+            }else{
+                System.out.print("False index: " + falseIndex + "   ");
+                //if the level is deemed unsafe, then remove the corrupt index and see if it's fixed:
+                int newLevelsList[] = new int[levels.length - 1];
+                int newLevelsListCounter = 0;
+                for(int i = 0; i < levels.length; i++){
+                    if(i != falseIndex){
+                        newLevelsList[newLevelsListCounter] = levels[i];
+                        newLevelsListCounter++;
+                    }
+                }
+
+                System.out.print("Before: " + Arrays.toString(levels)+ "   ");
+                System.out.print("To test again: " + Arrays.toString(newLevelsList));
+
+                boolean allDecreasing = true;
+                boolean allIncreasing = true;
+                previousLevel = 0;
+                for(int level : newLevelsList){
+                    if(previousLevel == 0){
+                        previousLevel = level;
+                    }else if(previousLevel == level){
+                        allDecreasing = false;
+                        allIncreasing = false;
+                    }else if(((previousLevel - level) >= 4) || ((level-previousLevel) >= 4)){
+                        allDecreasing = false;
+                        allIncreasing = false;
+                    }else if(previousLevel > level){
+                        allIncreasing = false;
+                    }else if(previousLevel < level){
+                        allDecreasing = false;
+                    }
+                    previousLevel = level;
+                }
+                if(allDecreasing || allIncreasing){
+                    safeLevels++;
+                    System.out.println("    PASS!");
+                }else{
+                    System.out.println("    FAIL!");
+                }
             }
         }
 
-        System.out.println(safeLevels);
+        System.out.println("Second part answer: " + safeLevels);
 
     }
 }
